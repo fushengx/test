aaa8591f2ab2e4a5080097f3ad8fdf8dcd0482b6
xingyu521 <1006796187@qq.com>
2019-07-04 10:21:01 +0800

    test

--- order.py
+++ order.py
@@ -1,3 +1,10 @@
+def sort(array):
+    for i in range(len(array)-1):
+        for j in range(len(array) -i -1):
+            if array[j] > array[j +1]:
+                array[j], array[j +1] = array[j +1], array[j]
+
 array = [5, 99, 2, 1, 33, 57, 6]
-array.sort()
+
+sort(array)
 print(array)
--- test/order.py
+++
@@ -1,3 +0,0 @@
-array = [5, 99, 2, 1, 33, 57, 6]
-array.sort()
-print(array)
